e7074e8a4a07b97a885418bcb9b532510f80b411
Benjamin Campa <benjamincampa@Benjamins-MacBook-Air.local>
2026-04-07 18:30:56 -0500

    Finished Deque ADT

--- Deque_ADT.py
+++ Deque_ADT.py
@@ -1,4 +1,33 @@
-class Deque: 
+class Deque:
     def __init__(self, capacity=5):
-       self._data =[]
-       self._capacity = capacity
+        self._data = []
+        self._capacity = capacity
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def is_full(self):
+        return len(self._data) >= self._capacity
+
+    def add_back(self, item):
+        if not self.is_full():
+            self._data.append(item)
+
+    def add_front(self, item):
+        if not self.is_full():
+            self._data.insert(0, item)
+
+    def remove(self, item):
+        if item in self._data:
+            self._data.remove(item)
+            return True
+        return False
+
+    def remove_back(self):
+        if not self.is_empty():
+            return self._data.pop()
+        return None
+
+    def __str__(self):
+        return str(self._data)
+    
