712ba035e920478eeaafbc4558ab6f1299257eb2
Benjamin Campa <benjamincampa@Benjamins-MacBook-Air.local>
2026-04-07 19:03:29 -0500

    Finished

--- event_scheduler.py
+++ event_scheduler.py
@@ -1,41 +1,46 @@
 from Deque_ADT import Deque
 from adt_arraystack import ArrayStack
 
-registration= Deque() 
-waitlist = ArrayStack() 
+registration = Deque()
+waitlist = ArrayStack()
 
-while True: 
-    command = input('Enter command: ')
+while True:
+    command = input("Enter command: ").strip().lower()
+
     if command == "list":
-        print ("Registered attendees: ", registration)
-        print ("Waitlist: ", waitlist.get_stack())
-    elif command.startswith("register "): 
-        name = command.split()[1] 
+        print("Registration List:", registration)
+        print("Waitlist:", waitlist.get_stack())
 
-        if not registration.is_full(): 
-            registration.add_back(name)  
-        else: 
+    elif command.startswith("register "):
+        name = command.split()[1]
+
+        if not registration.is_full():
+            registration.add_back(name)
+        else:
             waitlist.push(name)
 
-    elif command.startswith("vip "): 
-        name = command.split()[1] 
+    elif command.startswith("vip "):
+        name = command.split()[1]
 
-        if not registration.is_full(): 
-            registration.add_front(name)  
-        else: 
+        if not registration.is_full():
+            registration.add_front(name)
+        else:
             removed_person = registration.remove_back()
-            waitlist.push(removed_person)
-            registration.add_front(name) 
-    
-    elif command.startswith("cancel "): 
-        name = command.split()[1] 
+            if removed_person:
+                waitlist.push(removed_person)
+            registration.add_front(name)
 
-        if registration.remove(name): 
-            if not waitlist.is_empty(): 
-                next_person = waitlist.pop() 
-                registration.add_back(next_person) 
-        else: 
-            print(f"{name} is not found.") 
+    elif command.startswith("cancel "):
+        name = command.split()[1]
 
-    elif command == "exit": 
-        break 
+        if registration.remove(name):
+            if not waitlist.is_empty():
+                registration.add_back(waitlist.pop())
+        else:
+            print(f"{name} is not found.")
+
+    elif command == "exit":
+        break
+
+    else:
+        print("Invalid command.")
